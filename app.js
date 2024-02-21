2d6cc3daf401f08525ddab9ee03e61a0252707cd
Po1011 <qaws3588@gmail.com>
2024-02-21 15:28:08 +0800

    Apply on Animation

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-// let hero = document.querySelector(".hero")
-// let slider = document.querySelector(".slider")
-// let animation = document.querySelector("section.animation-wrapper")
+let hero = document.querySelector(".hero")
+let slider = document.querySelector(".slider")
+let animation = document.querySelector("section.animation-wrapper")
 
-// const time_line = new TimelineMax()
+const time_line = new TimelineMax()
 
-// // parameter1 是要控制的對象
-// // parameter2 是duration
-// // parameter3 是控制對象的原始狀態
-// // parameter4 是控制對象動畫結束後的狀態
-// // parameter5
-// time_line
-//     .fromTo(
-//         hero,
-//         1,
-//         { height: "0%" },
-//         { height: "100%", ease: Power2.easeInOut }
-//     )
-//     .fromTo(
-//         hero,
-//         1.2,
-//         { width: "80%" },
-//         { width: "100%", ease: Power2.easeInOut }
-//     )
-//     .fromTo(
-//         slider,
-//         1,
-//         { x: "-100%" },
-//         { x: "0%", ease: Power2.easeInOut },
-//         "-=1.2"
-//     )
-//     .fromTo(animation, 0.3, { opacity: 1 }, { opacity: 0 });
+// parameter1 是要控制的對象
+// parameter2 是duration
+// parameter3 是控制對象的原始狀態
+// parameter4 是控制對象動畫結束後的狀態
+// parameter5
+time_line
+    .fromTo(
+        hero,
+        1,
+        { height: "0%" },
+        { height: "100%", ease: Power2.easeInOut }
+    )
+    .fromTo(
+        hero,
+        1.2,
+        { width: "80%" },
+        { width: "100%", ease: Power2.easeInOut }
+    )
+    .fromTo(
+        slider,
+        1,
+        { x: "-100%" },
+        { x: "0%", ease: Power2.easeInOut },
+        "-=1.2"
+    )
+    .fromTo(animation, 0.3, { opacity: 1 }, { opacity: 0 });
 
-// setTimeout(() => {
-//     animation.style.pointerEvents = "none";
-// }, 2500);
+setTimeout(() => {
+    animation.style.pointerEvents = "none";
+}, 2500);
 
 // 讓整個網站的ENTER KEY都無法使用
 window.addEventListener("keypress", (e) => {
